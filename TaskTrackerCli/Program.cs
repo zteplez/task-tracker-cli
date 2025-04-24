60253a9cd6955fd6486d3fc9e68614e518107f98
zteplez <asibrahim98@gmail.com>
2025-04-24 11:05:16 +0300

    [feat]: Add handling args and print help message

---
+++ TaskTrackerCli/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello CLI!");
+
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Empty CLI command.");
+            return;
+        }
+        else
+        {
+            LogArgs(args);
+            string firstCommand = args[0].ToLower();
+
+            switch (firstCommand)
+            {
+                case "add":
+                    break;
+                case "update":
+                    break;
+                case "delete":
+                    break;
+                case "mark-in-progress":
+                    break;
+                case "mark-done":
+                    break;
+                case "list":
+                    break;
+                case "help":
+                    break;
+                default:
+                    ShowHelp();
+                    break;
+            }
+        }
+    }
+    public static void LogArgs(string[] args)
+    {
+        foreach (string arg in args)
+        {
+            Console.WriteLine($"Arg value -> {arg} \n");
+        }
+    }
+    public static void ShowHelp()
+    {
+        Console.WriteLine("Usage:");
+        Console.WriteLine("  add <task>          Add new task.");
+        Console.WriteLine("  list                List all tasks.");
+        Console.WriteLine("  remove <id>         Delete specified task with ID");
+        Console.WriteLine("  -h, --help          Shows help.");
+    }
+
+
+}
