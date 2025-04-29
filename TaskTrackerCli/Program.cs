6d768b146daa54966b79a45697ed9936b3adc521
zteplez <asibrahim98@gmail.com>
2025-04-29 11:50:31 +0300

    [feat]: Add update and add command handler

--- TaskTrackerCli/Program.cs
+++ TaskTrackerCli/Program.cs
@@ -2,6 +2,7 @@
 
 class Program
 {
+    public static List<string> tasks = new List<string>();
     static void Main(string[] args)
     {
         Console.WriteLine("Hello CLI!");
@@ -13,14 +14,16 @@ class Program
         }
         else
         {
-            LogArgs(args);
+            //LogArgs(args);
             string firstCommand = args[0].ToLower();
 
             switch (firstCommand)
             {
                 case "add":
+                    HandleAddCommand(args);
                     break;
                 case "update":
+                    HandleUpdateCommand(args);
                     break;
                 case "delete":
                     break;
@@ -49,10 +52,43 @@ class Program
     {
         Console.WriteLine("Usage:");
         Console.WriteLine("  add <task>          Add new task.");
+        Console.WriteLine("  add <task> <description>          Add new task with description.");
         Console.WriteLine("  list                List all tasks.");
         Console.WriteLine("  remove <id>         Delete specified task with ID");
         Console.WriteLine("  -h, --help          Shows help.");
     }
+    public static void HandleAddCommand(string[] args)
+    {
+        switch (args.Length)
+        {
+            case 2:
+                tasks.Add(args[1]);
+                Console.WriteLine($"Add {args[1]}.");
+                break;
+            case 3:
+                tasks.Add(args[1]); // also pass description
+                Console.WriteLine($"Add {args[1]}.");
+                break;
+            default:
+                Console.WriteLine("Wrong use case, please check for --help.");
+                break;
+        }
+    }
+    public static void HandleUpdateCommand(string[] args)
+    {
+        if (args.Length < 3)
+        { // Checks if task id and value passed or empty
+            Console.WriteLine("Missing id or new task value ");
+            return;
+        }
+        if (args.Length > 3)
+        {
+            Console.WriteLine("You should pass just id and new task value");
+            return;
+        }
+        // Check all tasks id and compare the passaed value if you find the task object store here.
+        // if you cant find task id then print a message that task doesnt exist.
+        // assign a new value task object.
 
-
+    }
 }
